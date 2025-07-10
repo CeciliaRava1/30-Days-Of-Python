e41622191ff2bb9993b3c3935362f10a4cb9a2d9
cecilia <ravacecilia1@gmail.com>
2025-07-10 08:28:56 -0300

    feat: Modify file structure

--- 01_helloWorld.py
+++ 01_helloWorld.py
@@ -1,3 +1,30 @@
+# Single line comment
+
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
+
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
 """Exercise: Level 2
 1. Check the python version you are using
 2. Open the python interactive shell and do the following operations. The operands are 

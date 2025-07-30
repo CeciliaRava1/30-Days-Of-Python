0f86dfbf2ee79bdfcc6d75f639e4b1635fabbab8
cecilia <ravacecilia1@gmail.com>
2025-07-30 13:27:20 -0300

    feat: Complete day 15

---
+++ 15_type_errors.py
@@ -0,0 +1,41 @@
+### type errors ###
+
+'''
+💻 Exercises: Day 15
+Open you python interactive shell and try all the examples covered in this section.
+🎉 CONGRATULATIONS ! 🎉
+'''
+
+# - - SyntaxError
+# print 'hello world'
+
+# - - NameError
+# print(age)
+
+# - - IndexError
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers[5])
+
+# - - ModuleNotFoundError
+# import maths
+
+# - - AttributeError
+# import math
+# math.PI
+
+# - - KeyError
+# users = {'name':'Asab', 'age':250, 'country':'Finland'}
+# users['county']
+
+# - - TypeError
+# 4 + '3'
+
+# - - ImportError
+# from math import power
+
+# - - ValueError
+# int('12a')
+
+# - - ZeroDivisionError
+# 1/0
+
